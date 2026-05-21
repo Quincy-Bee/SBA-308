e74121bee7b0f77666144d4c333ef843e6d6408b
Quincy-Bee <quincybee@icloud.com>
2026-05-20 20:57:56 -0400

    second commit

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const AssignmentGroup = {
       due_at: "2023-01-25",
       points_possible: 50
     },
-
     {
       id: 2,
       name: "Write a Function",
@@ -77,69 +76,26 @@ const LearnerSubmissions = [
   }
 ];
 
-const ids = [125, 132];
-
-// 2. create learner objects
-for (let learnerId of ids) {
-  console.log(learnerId)
-
-  /*
-  for(every value in submissions){
-  does the learner id match?
-      yes:
-          add to the object
-      no:
-  }       skip it
-  }
-  */  
-
-  // need a variable to store totals.
-  let score = 0
-
-  for(let i = 0; i < LearnerSubmissions.length; i++){
-
-    if (learnerId === LearnerSubmissions[i].learner_id){
-      console.log(LearnerSubmissions[i].submission.score)
-
+function getLearnerData(course, ag, submissions) {
+  // here, we would process this data to achieve the desired result.
+  const result = [
+    {
+      id: 125,
+      avg: 0.985, // (47 + 150) / (50 + 150)
+      1: 0.94, // 47 / 50
+      2: 1.0 // 150 / 150
+    },
+    {
+      id: 132,
+      avg: 0.82, // (39 + 125) / (50 + 150)
+      1: 0.78, // 39 / 50
+      2: 0.833 // late: (140 - 15) / 150
     }
+  ];
 
-  }
-
-  //submission [0]
-
-  let learnerReport = {
-    id: learnerId
-  }
-
-  console.log(learnerReport)
-
+  return result;
 }
 
-// function getLearnerData(course, ag, submissions) {
-//   // here, we would process this data to achieve the desired result.
-//   const result = [
-//     {
-//       id: 125,
-//       avg: 0.985, // (47 + 150) / (50 + 150)
-//       1: 0.94, // 47 / 50
-//       2: 1.0 // 150 / 150
-//     },
-//     {
-//       id: 132,
-//       avg: 0.82, // (39 + 125) / (50 + 150)
-//       1: 0.78, // 39 / 50
-//       2: 0.833 // late: (140 - 15) / 150
-//     }
-//   ];
+const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 
-//   return result;
-// }
-
-// const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-
-// console.log(result);
-
-let learnerID1 = 125;
-let learnerID2 = 132;
-
-console.log(learnerID1, learnerID2)
+console.log(result);
