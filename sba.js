90696b4eab50713af2515798fb5c37f1d548f2fa
Quincy-Bee <quincybee@icloud.com>
2026-05-20 21:40:55 -0400

    3rd commit

--- sba.js
+++ sba.js
@@ -34,7 +34,7 @@ const AssignmentGroup = {
 };
 
 // The provided learner submission data.
-const LearnerSubmissions = [
+const LearnerSub = [
   {
     learner_id: 125,
     assignment_id: 1,
@@ -77,11 +77,13 @@ const LearnerSubmissions = [
   }
 ];
 
+
 const ids = [125, 132];
 
 // 2. create learner objects
 for (let learnerId of ids) {
-  console.log(learnerId)
+  console.log("Learner id: " +learnerId)
+
 
   /*
   for(every value in submissions){
@@ -100,10 +102,13 @@ for (let learnerId of ids) {
 
     if (learnerId === LearnerSubmissions[i].learner_id){
       console.log(LearnerSubmissions[i].submission.score)
+      score += LearnerSubmissions[i].submission.score
+      console.log(ag.assignments[0].points_possible)
 
     }
 
   }
+  console.log("total score: " +score)
 
   //submission [0]
 
@@ -116,30 +121,44 @@ for (let learnerId of ids) {
 }
 
 // function getLearnerData(course, ag, submissions) {
-//   // here, we would process this data to achieve the desired result.
-//   const result = [
-//     {
-//       id: 125,
-//       avg: 0.985, // (47 + 150) / (50 + 150)
-//       1: 0.94, // 47 / 50
-//       2: 1.0 // 150 / 150
-//     },
-//     {
-//       id: 132,
-//       avg: 0.82, // (39 + 125) / (50 + 150)
-//       1: 0.78, // 39 / 50
-//       2: 0.833 // late: (140 - 15) / 150
-//     }
-//   ];
+// //   // here, we would process this data to achieve the desired result.
+  
 
-//   return result;
-// }
+// array of learner report objects
+  // const result = []
 
-// const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+// keep track of learner ids
+  // const ids =[]
+
+  // 1. find unique learners (figuring out hw many learners there are (and how many report objects we need))
+for (let sub of submissions){
+  //check for the learner id in the array (make sure their unique
+  if (ids.includes(sub.learner_id) !==true) {
+    ids.push(sub.learner_id); 
+  }
+}
+  // //     {
+// //       id: 125,
+// //       avg: 0.985, // (47 + 150) / (50 + 150)
+// //       1: 0.94, // 47 / 50
+// //       2: 1.0 // 150 / 150
+// //     },
+// //     {
+// //       id: 132,
+// //       avg: 0.82, // (39 + 125) / (50 + 150)
+// //       1: 0.78, // 39 / 50
+// //       2: 0.833 // late: (140 - 15) / 150
+// //     }
+// //   ];
+
+  return result;
+}
+
+const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSub);
 
 // console.log(result);
 
-let learnerID1 = 125;
-let learnerID2 = 132;
+// // let learnerID1 = 125;
+// // let learnerID2 = 132;
 
-console.log(learnerID1, learnerID2)
+// // console.log(learnerID1, learnerID2)
